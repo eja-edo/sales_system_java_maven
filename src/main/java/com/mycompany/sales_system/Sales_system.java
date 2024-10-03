7689ce38bf3af93a047a0aedfce805f1e6207db5
edo_eja <duyanhsadg@gmail.com>
2024-10-03 21:38:49 +0700

    first commit

---
+++ src/main/java/com/mycompany/sales_system/Sales_system.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sales_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author duyan
+ */
+public class Sales_system {
+
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=sales_management_db;encrypt=false","sa","12345678");
+                        System.out.println("ket noi thanh cong!!!");
+        }catch(ClassNotFoundException | SQLException ex){
+        System.out.println("loi" + ex.getMessage());
+    }
+    }
+}
