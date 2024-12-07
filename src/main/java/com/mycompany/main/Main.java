09cd8f840adc6d07505a3d4348380200e876e0e7
duongbill <billduongg@gmail.com>
2024-12-08 00:55:41 +0700

    Lưu thay đổi tạm thời

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -33,7 +33,6 @@ public class Main extends javax.swing.JFrame implements CardSelectionListener, U
     menu1.setParent(this);
     trang_con1.setTrangCon(1);
     trang_con2.setTrangCon(2);
-
     trang_con3.setTrangCon(3);
     trang_con4.setTrangCon(4);
 
